ea8bcb73570c888d691a59e8f906e8eace51661d
choraeng <cyh905@nate.com>
2022-08-11 22:57:25 +0900

    .

---
+++ photolock/photoLock/photoLock/photoLock/Views/Main/mainView.swift
@@ -0,0 +1,28 @@
+//
+//  mainView.swift
+//  photoLock
+//
+//  Created by user on 2022/08/11.
+//
+
+import SwiftUI
+
+struct mainView: View {
+    var body: some View {
+        ZStack{ // for detail page or sheet
+            //MARK: - navigation bar
+            
+            //MARK: - toolbar
+            
+            //MARK: - tab bar
+            
+            //MARK: - item list
+        }
+    }
+}
+
+struct mainView_Previews: PreviewProvider {
+    static var previews: some View {
+        mainView()
+    }
+}
