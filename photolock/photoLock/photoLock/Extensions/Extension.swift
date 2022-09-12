a7704b059647b723bba8ce551b7fef3bc2def8f2
choraeng <cyh905@nate.com>
2022-09-13 01:05:08 +0900

    라이브러리 추가(이미지 크기 관련 이슈로 인하여서 코드 수정한 버전, YPLibraryVC - targetSize 함수 수정(line 572,573 ))

---
+++ photolock/photoLock/photoLock/Extensions/Extension.swift
@@ -0,0 +1,8 @@
+//
+//  Extension.swift
+//  photoLock
+//
+//  Created by 조영훈 on 2022/09/11.
+//
+
+import Foundation
---
+++ photolock/photoLock/photoLock/Views/Commons/emtpyView.swift
@@ -0,0 +1,20 @@
+//
+//  emtpyView.swift
+//  photoLock
+//
+//  Created by 조영훈 on 2022/09/10.
+//
+
+import SwiftUI
+
+struct emtpyView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct emtpyView_Previews: PreviewProvider {
+    static var previews: some View {
+        emtpyView()
+    }
+}
